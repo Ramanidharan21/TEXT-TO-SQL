474f0b34055aa33db2238f3d7d1cbaad7e2e41e5
Ramanidharan21 <ramanidharan21@gmail.com>
2025-10-23 14:18:23 +0530

    fix the port issue

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def home():
 def health_check():
     return {"status": "ok"}
 
-# if __name__=="__main__":
-#     import uvicorn
-#     port = int(os.getenv("PORT", 8000)) 
-#     uvicorn.run(app, host="0.0.0.0", port=port)
+if __name__=="__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 7860)) 
+    uvicorn.run(app, host="0.0.0.0", port=port)
